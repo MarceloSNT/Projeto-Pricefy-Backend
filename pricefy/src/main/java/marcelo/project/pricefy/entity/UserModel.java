9575ea264c0a6c890401887f9b7b8804a200db72
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-20 12:23:20 -0300

    Feat: Adding UserModel

--- pricefy/src/main/java/marcelo/project/pricefy/entity/UserModel.java
+++ pricefy/src/main/java/marcelo/project/pricefy/entity/UserModel.java
@@ -1,4 +1,24 @@
 package marcelo.project.pricefy.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "TBUSER")
 public class UserModel {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUser;
+
+    private String dsUsername;
+
+    private String dsEmail;
+
+    private String dsPassword;
+
 }
