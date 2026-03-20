32a0f8e8fc05b86f8ab347e243b3c61a0dffcb48
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-20 13:53:11 -0300

    Feat: Adding register of user

--- pricefy/src/main/java/marcelo/project/pricefy/Controller/UserController.java
+++ pricefy/src/main/java/marcelo/project/pricefy/Controller/UserController.java
@@ -1,4 +1,33 @@
 package marcelo.project.pricefy.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import marcelo.project.pricefy.dto.request.UserRequestDto;
+import marcelo.project.pricefy.dto.response.UserResponseDto;
+import marcelo.project.pricefy.entity.UserModel;
+import marcelo.project.pricefy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@Tag(name = "Usuário", description = "Endpoints para usuário")
+@RequestMapping("pricefy/user/")
 public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    @PostMapping("register")
+    @Operation(summary = "Cadastro de usuário",description = "Realiza o cadastro do usuário")
+    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRequestDto userRequest){
+        UserResponseDto user = userService.createUser(userRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
--- pricefy/src/main/java/marcelo/project/pricefy/service/UserService.java
+++ pricefy/src/main/java/marcelo/project/pricefy/service/UserService.java
@@ -1,4 +1,35 @@
 package marcelo.project.pricefy.service;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import marcelo.project.pricefy.dto.request.UserRequestDto;
+import marcelo.project.pricefy.dto.response.UserResponseDto;
+import marcelo.project.pricefy.entity.UserModel;
+import marcelo.project.pricefy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResponseDto createUser(UserRequestDto userRequest){
+        UserModel user = new UserModel();
+
+        user.setIdUser(userRequest.idUser());
+        user.setDsUsername(userRequest.dsUsername());
+        user.setDsEmail(userRequest.dsEmail());
+        user.setDsPassword(userRequest.dsPassword());
+
+        UserModel userSaved = userRepository.save(user);
+
+        return new UserResponseDto(
+                userSaved.getIdUser(),
+                userSaved.getDsUsername(),
+                userSaved.getDsEmail(),
+                userSaved.getDsPassword()
+        );
+    }
 }
