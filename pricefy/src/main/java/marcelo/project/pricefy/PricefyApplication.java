fa4e837016245a487b65af0363b0ed8ff471a85a
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-20 12:22:38 -0300

    Doc: Initial unversioned files

---
+++ pricefy/src/main/java/marcelo/project/pricefy/PricefyApplication.java
@@ -0,0 +1,13 @@
+package marcelo.project.pricefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricefyApplication.class, args);
+	}
+
+}
---
+++ pricefy/src/test/java/marcelo/project/pricefy/PricefyApplicationTests.java
@@ -0,0 +1,13 @@
+package marcelo.project.pricefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
