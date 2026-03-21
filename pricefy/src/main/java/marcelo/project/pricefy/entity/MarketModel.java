1528b3fd9cfe733b727b2d991bc871396c057b65
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-21 19:41:52 -0300

    Feat: Adding Create Market Model

--- pricefy/src/main/java/marcelo/project/pricefy/entity/MarketModel.java
+++ pricefy/src/main/java/marcelo/project/pricefy/entity/MarketModel.java
@@ -1,4 +1,22 @@
 package marcelo.project.pricefy.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TBMARKET")
 public class MarketModel {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idMarket;
+
+    private String dsName;
+
+    @ManyToOne
+    private UserModel user;
 }
