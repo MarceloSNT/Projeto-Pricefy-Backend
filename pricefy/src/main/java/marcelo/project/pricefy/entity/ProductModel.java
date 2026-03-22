055f21593f54bf7ef4aaab2be0964c5d97e30556
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-22 14:36:57 -0300

    Feat: Adding productModel

--- pricefy/src/main/java/marcelo/project/pricefy/entity/ProductModel.java
+++ pricefy/src/main/java/marcelo/project/pricefy/entity/ProductModel.java
@@ -1,4 +1,23 @@
 package marcelo.project.pricefy.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "TBPRODUCT")
 public class ProductModel {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+
+    private String dsName;
+
+    @ManyToOne
+    private UserModel user;
 }
