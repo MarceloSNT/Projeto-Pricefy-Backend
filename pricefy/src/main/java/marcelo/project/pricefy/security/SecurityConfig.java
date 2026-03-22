32dc6b8713194574261ef7abd940e91f449514cd
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-22 01:50:54 -0300

    Fix: Changing permissions for anyRequests to just athenticated and fix path request of login and register

--- pricefy/src/main/java/marcelo/project/pricefy/security/SecurityConfig.java
+++ pricefy/src/main/java/marcelo/project/pricefy/security/SecurityConfig.java
@@ -34,8 +34,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
-                        .requestMatchers(HttpMethod.POST,"auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST,"auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/pricefy/user/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/pricefy/user/register").permitAll()
 
                         .requestMatchers(
                                 "/swagger-ui/**",
@@ -46,7 +46,7 @@ public class SecurityConfig {
                                 "/webjars/**"
                         ).permitAll()
 
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
