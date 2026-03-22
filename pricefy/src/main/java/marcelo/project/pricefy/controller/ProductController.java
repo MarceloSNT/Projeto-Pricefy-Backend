59a7a978fa7d3e22b0887fe1d8349d7c1270e4ed
Marcelo <marcelodossantosmachado05@gmail.com>
2026-03-22 14:37:47 -0300

    Feat: Adding creating of product

--- pricefy/src/main/java/marcelo/project/pricefy/controller/ProductController.java
+++ pricefy/src/main/java/marcelo/project/pricefy/controller/ProductController.java
@@ -1,4 +1,33 @@
 package marcelo.project.pricefy.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import marcelo.project.pricefy.dto.request.product.ProductRequestDto;
+import marcelo.project.pricefy.dto.response.product.ProductResponseDto;
+import marcelo.project.pricefy.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@AllArgsConstructor
+@Tag(name = "Produto", description = "Endpoints para mercado")
+@RequestMapping("pricefy/product/")
 public class ProductController {
+
+    @Autowired
+    private final ProductService productService;
+
+    @PostMapping("create/{idUser}")
+    @Operation(summary = "Criação de produto", description = "Criar produto")
+    @PreAuthorize("#idUser == authentication.principal.idUser")
+    public ResponseEntity<ProductResponseDto> createProoduct(@Valid @RequestBody ProductRequestDto productRequestDto, @PathVariable Long idUser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(productRequestDto, idUser));
+    }
 }
--- pricefy/src/main/java/marcelo/project/pricefy/service/ProductService.java
+++ pricefy/src/main/java/marcelo/project/pricefy/service/ProductService.java
@@ -1,4 +1,37 @@
 package marcelo.project.pricefy.service;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import marcelo.project.pricefy.dto.request.product.ProductRequestDto;
+import marcelo.project.pricefy.dto.response.product.ProductResponseDto;
+import marcelo.project.pricefy.entity.ProductModel;
+import marcelo.project.pricefy.entity.UserModel;
+import marcelo.project.pricefy.repository.ProductRepository;
+import marcelo.project.pricefy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public ProductResponseDto createProduct(ProductRequestDto productRequestDto, Long idUser){
+        UserModel user = userRepository.findByIdUser(idUser);
+        ProductModel product = new ProductModel();
+
+        product.setIdProduct(productRequestDto.idProduct());
+        product.setDsName(productRequestDto.dsName());
+        product.setUser(user);
+
+        ProductModel productSaved = productRepository.save(product);
+
+        return new ProductResponseDto(
+                productSaved.getIdProduct(),
+                productSaved.getDsName(),
+                productSaved.getUser().getDsUsername()
+        );
+    }
 }
